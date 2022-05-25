06ff7f20a06c827bcb626998c29a452e497352cc
Paolo Pietroluongo <paolo@pietroluongo>
2022-05-25 10:03:05 -0400

    Initial commit V.00

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'philipstv-iob adapter settings': {
+        'en': 'Adapter settings for philipstv-iob',
+        'de': 'Adaptereinstellungen für philipstv-iob',
+        'ru': 'Настройки адаптера для philipstv-iob',
+        'pt': 'Configurações do adaptador para philipstv-iob',
+        'nl': 'Adapterinstellingen voor philipstv-iob',
+        'fr': "Paramètres d'adaptateur pour philipstv-iob",
+        'it': "Impostazioni dell'adattatore per philipstv-iob",
+        'es': 'Ajustes del adaptador para philipstv-iob',
+        'pl': 'Ustawienia adaptera dla philipstv-iob',
+        'zh-cn': 'philipstv-iob的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
